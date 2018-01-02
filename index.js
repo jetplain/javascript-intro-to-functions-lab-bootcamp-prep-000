c28432289e6a3371d1b0d5e052d7d4f6ae7ad42b
Jaime Taylor <mrsjaimetaylor@gmail.com>
2018-01-02 20:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function logWhisper(string)
 
 function sayHiToGrandma(string) 
 {
-  if string === string.toLowerCase
-  return ("I can't hear you!")
-  
+  if (string) === string.toLowerCase;
+  return ("I can't hear you!");
+
   else 
-  return ("YES INDEED!")
+  return ("YES INDEED!");
 }
 
 
