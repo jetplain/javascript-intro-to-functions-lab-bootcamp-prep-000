46bf6c0bf6943c11c90665c92d1687e99a474efe
Jaime Taylor <mrsjaimetaylor@gmail.com>
2018-01-02 18:13:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  console.log.toUpperCase()
+} 
+
+function logWhisper(string)
+{ 
+  console.log()
+}
+
+function sayHiToGrandma(string) 
+{
+  if string === string.toLowerCase
+  return ("I can't hear you!")
+  
+  else 
+  return ("YES INDEED!")
+}
+
+
